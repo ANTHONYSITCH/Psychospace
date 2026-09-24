1e1a735731afc0e42be9e1b3934088865ff82870
SITCH SENPAI <anthonysitcheping@gmail.com>
2026-09-24 09:22:25 +0200

    modif front et back

--- app/main.py
+++ app/main.py
@@ -14,12 +14,7 @@ app = FastAPI(title="Psychospace API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1):(\d+)",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
